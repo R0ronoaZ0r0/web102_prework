7c5ca226181fec39ff8a221e7bd18fcec1eb6e32
R0ronoaZ0r0 <hemanthredi@gmail.com>
2023-08-06 01:36:54 -0600

    Challenge 3 completed

--- index.js
+++ index.js
@@ -29,27 +29,33 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (let i = 0; i < games.length; i++) {
 
         // create a new div element, which will become the game card
-
+        const game = document.createElement("div");
 
         // add the class game-card to the list
-
+        game.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
+        game.innerHTML = `<img class="game-img" src=${games[i]["img"]} >
+            \nThe game "${games[i]["name"]}" 
+            is ${games[i]["description"]}.
+            It has ${games[i]["backers"]} backers and
+            it has raised $${games[i]["pledged"]} of its $${games[i]["goal"]} goal.`;
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
 
-
+        gamesContainer.appendChild(game);
         // append the game to the games-container
+    }
 
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
